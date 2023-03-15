6b4b3604bce4622a96cf15c3bcd4d5cb1f3a9ef3
pawelbo231 <pawelbo231@gmail.com>
2023-03-15 23:28:46 +0100

    closing logic

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,9 +1,16 @@
 #include <string>
 #include "Application.h"
 
-std::string Application::parseRequest(std::string request){
+std::string Application::parseRequest(std::string request)
+{
+    if (request.compare("exit") == 0)
+    {
+        m_shouldTerminate = true;
+        return "Terminating";
+    };
     return "Request: " + request;
 };
-bool Application::isFinished(){
-    return true;
+bool Application::isFinished()
+{
+    return !m_shouldTerminate;
 }
--- src/Application.h
+++ src/Application.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <string>
-class Application{
-    public:
-        std::string parseRequest(std::string request);
-        bool isFinished();
+class Application
+{
+public:
+    std::string parseRequest(std::string request);
+    bool isFinished();
+
+private:
+    bool m_shouldTerminate = false;
 };
