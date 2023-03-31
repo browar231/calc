3dd0f8880235eb3bb620b2f2f8b4c8ffd8e33eed
pawelbo231 <pawelbo231@gmail.com>
2023-03-31 17:08:14 +0200

    old, unused code

--- src/main.cpp
+++ src/main.cpp
@@ -66,28 +66,3 @@ bool Calculator::OnInit()
 
 	return true;
 }
-// #include "Application.h"
-// #include "Common.h"
-
-// int main()
-// {
-// 	bool running { true };
-// 	std::cout << "Please type your request without spaces, allowed operations: +-*/^"
-// 			  << std::endl;
-// 	std::cout << "Use dot for fractions ie. 10.32+12" << std::endl;
-// 	std::cout << "Type exit to close" << std::endl;
-// 	while (running) {
-// 		std::string input = Application::getInputFromConsole();
-// 		if (input == "exit") {
-// 			running = false;
-// 			break;
-// 		}
-// 		try {
-// 			std::string answer = Application::parseRequest(input);
-// 			std::cout << "Answer: " << answer << std::endl;
-// 		} catch (std::runtime_error& error) {
-// 			std::cout << error.what() << std::endl;
-// 		}
-// 	}
-// 	return 0;
-// }
